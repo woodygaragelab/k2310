c2a65bc19ecf186ac1041dda8c1d5dea3831aa03
woodygaragelab <woodygaragelab@gmail.com>
2021-04-03 20:03:37 +0900

    add api

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,13 @@ const awsmobile = {
     "aws_cognito_region": "ap-northeast-1",
     "aws_user_pools_id": "ap-northeast-1_plXzAdlHa",
     "aws_user_pools_web_client_id": "2va5729ehfj97clm8l1s3ms888",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://hlew6jnsczgqbede4ntbmrcnue.appsync-api.ap-northeast-1.amazonaws.com/graphql",
+    "aws_appsync_region": "ap-northeast-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-47enftpfqjggvc6wpqjldcjfre",
+    "aws_user_files_s3_bucket": "k231062400-staging",
+    "aws_user_files_s3_bucket_region": "ap-northeast-1"
 };
 
 
