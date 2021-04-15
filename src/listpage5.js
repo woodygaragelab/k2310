774a2d4eeafd22ee09f6143983c9f286d391e99a
woodygaragelab <woodygaragelab@gmail.com>
2021-04-15 14:07:14 +0900

    m5

--- src/listpage5.js
+++ src/listpage5.js
@@ -2,29 +2,20 @@ import React from 'react';
 import { Component } from 'react';
 import './App.css';
 import './listpage.css';
-// import { API } from 'aws-amplify';
-// import { deleteBook as deleteBookMutation } from './graphql/mutations';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { withRouter } from 'react-router-dom';
-//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-//import { faEdit,faTrash } from "@fortawesome/free-solid-svg-icons";
-//import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
-//import { faHeart, faHome, faChartLine } from "@fortawesome/free-solid-svg-icons";
 
-const initialItemState = [
-  { key:'9901', sortkey:'1', group: '99', name: '土', description: '作業91',data:'{"total":9}' },
-  { key:'9902', sortkey:'2', group: '99', name: '日', description: '作業92',data:'{"total":8}' },
-]
+const initialItemState = [ { key:'99', sortkey:'1', group: '99', name: '土', description: '91',data:'{"total":9}'}]
 
 class ListPage extends Component {
 
   constructor(props){
     super(props);
     this.fetchItemsFromAPI = this.fetchItemsFromAPI.bind(this);
-    this.selectM4 = this.selectM4.bind(this);
     this.selectM5 = this.selectM5.bind(this);
     this.selectM6 = this.selectM6.bind(this);
+    this.selectM7 = this.selectM7.bind(this);
+    this.selectM8 = this.selectM8.bind(this);
     this.state = {
       items: initialItemState,
       month : "5"
@@ -47,27 +38,22 @@ class ListPage extends Component {
   }
 
   editItem(item) {
-    this.props.history.push({
-      pathname: '/detailpage',
-      state: { month:this.state.month, item: item }
-    });
+    this.props.history.push({ pathname: '/detailpage', state: { month:this.state.month, item: item }  });
   }
 
-  selectM4() { this.props.history.push({ pathname: '/listpage4' }); }
-
-  selectM5() { this.fetchItemsFromAPI("5"); }
-
-  selectM6() { this.props.history.push({ pathname: '/listpage6' }); }
+  selectM5() {  this.props.history.push({ pathname: '/listpage5' });  }
+  selectM6() {  this.props.history.push({ pathname: '/listpage6' });  }
+  selectM7() {  this.props.history.push({ pathname: '/listpage7' });  }
+  selectM8() {  this.props.history.push({ pathname: '/listpage8' });  }
 
   render() {
     return (
       <div className="mt-5 mb-5 container-fluid bg-color-1">
-        <header className="fixed-top siteHeader">
-            <div onClick={this.selectM4} className="col-2">4月</div>
-            <div onClick={this.selectM5} className="col-2 siteHeaderSelected">5月</div>
+        <header className="fixed-top siteHeader k2310BgM5">
+            <div onClick={this.selectM5} className="col-2 k2310FgM5">5月</div>
             <div onClick={this.selectM6} className="col-2">6月</div>
-            <div className="col-2">7月</div>
-            <div className="col-2">8月</div>
+            <div onClick={this.selectM7} className="col-2">7月</div>
+            <div onClick={this.selectM8} className="col-2">8月</div>
         </header>
 
         <form>
@@ -79,19 +65,19 @@ class ListPage extends Component {
               <div className="card" key={item.key + item.sortkey}>
                 <div className="card-body bg-color-2" onClick={() => this.editItem(item)}>
                   <div className="row">
-                    <div className="col-3">
-                      <div><h4>{item.sortkey}({item.name})</h4></div>
-                    </div>
-                    <div className="col-3">
-                      <div><h4>{item.description}</h4></div>
+                    <div className="col-4">
+                      <div><h4>{item.sortkey}({item.name})</h4></div>    {/* 日（曜） */}
                     </div>
                     <div className="col-4">
+                      <div><h4>{item.description}</h4></div>              {/* 宿泊者 */}
+                    </div>
+                    <div className="col-4">                   {/* 人数（朝-昼-夕-泊) */}
                       <div><h4>{itemdata.total}({itemdata.c1}-{itemdata.c2}-{itemdata.c3}-{itemdata.c4})</h4></div>
                     </div>
                   </div>              
                 </div>
-              </div>  
-            )              
+              </div>              
+            )
           })
         }
         </form>
